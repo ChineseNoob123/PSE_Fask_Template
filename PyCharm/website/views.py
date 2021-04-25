de7f360c68444cbcaf2eaebf8d952267e1066499
Geekkon <geckobaum@gmail.com>
2021-04-25 20:17:22 +0200

    New Home Page with notes

--- PyCharm/website/views.py
+++ PyCharm/website/views.py
@@ -1,13 +1,13 @@
 # Store the default Routes that will be usable by the server
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import login_required, current_user
 
 views = Blueprint('views', __name__)
 
 
 # Homepage
-@views.route('/')
+@views.route('/', methods=['GET','POST'])
 @login_required
 def home():
-    return render_template("base.html", user=current_user)
+    return render_template("home.html", user=current_user)
