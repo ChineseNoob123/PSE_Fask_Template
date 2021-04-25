dd2c80a9d301444ce251d2b8dfde43ee6c1d41f5
Geekkon <geckobaum@gmail.com>
2021-04-25 20:13:47 +0200

    Adjusted Navbar to show only viable options
    Only show home if you are logged in

--- PyCharm/website/views.py
+++ PyCharm/website/views.py
@@ -10,4 +10,4 @@ views = Blueprint('views', __name__)
 @views.route('/')
 @login_required
 def home():
-    return render_template("base.html")
+    return render_template("base.html", user=current_user)
